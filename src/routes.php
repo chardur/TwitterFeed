ffe073f4bcd0e4a2979f285e05d95f05c57ec3bb
Charles Durfee <charlesdurfee@gmail.com>
2018-08-04 16:57:19 -0600

    created an index.phtml file in the public directory to use as a main page, tested some come in routes and settings

--- src/routes.php
+++ src/routes.php
@@ -8,5 +8,15 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Sample log message
     $this->logger->info("tweet-away '/' route");
     // Render index view
+    echo "testing routes";
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/test', function (Request $request, Response $response) {
+    // Sample log message
+    $this->logger->info("tweet-away '/test' route");
+    // Render index view
+    echo "testing";
+    //return $this->renderer->render("testing");
+});
+
--- src/settings.php
+++ src/settings.php
@@ -7,8 +7,8 @@ return [
         'db' => [
             'host' => '67.205.183.11',
             'port' => '3306',
-            'dbname' => 'feed_donstringham',
-            'username' => 'donstringham',
+            'dbname' => 'feed_chardur',
+            'username' => 'chardur',
             'password' => 'changeme'
         ],
         // Monolog settings
@@ -19,7 +19,7 @@ return [
         ],
         // Renderer settings
         'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
+            'template_path' => __DIR__ . '/../public/',
         ]
     ],
 ];
